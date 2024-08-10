6dff027dfd100274c893c9c8b9dd6d985cb0def9
Ricky Liao <liaoric@uw.edu>
2024-08-10 16:45:54 -0700

    setup storage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,25 @@
 import React from 'react';
 import { ReactFlowProvider } from '@xyflow/react';
 import FlowChart from '@/components/FlowChart'
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { redirect } from 'next/navigation';
 
-const App: React.FC = () => (
-  <ReactFlowProvider>
-    <FlowChart />
-  </ReactFlowProvider>
-);
+const App: React.FC = () => {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+  
+  console.log(user);
+  if (!user) {
+    redirect('/api/auth/login');
+  }
+
+  return (
+    <ReactFlowProvider>
+      <FlowChart />
+    </ReactFlowProvider>
+  )
+};
 
 export default App;
